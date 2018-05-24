8efeedd415c10c3e5901ad796deb3bc0aac424fd
Sten Duindam <sten@stenduindam.nl>
2018-05-24 11:50:08 +0200

    BUILD_ALPHA01

---
+++ Assets/Scenes/ScenesSten/Scripts/Boss/BossController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour {
+
+    private SpriteRenderer currSprite;
+    private Color currSpriteColor;
+
+	// Use this for initialization
+	void Start () {
+        currSprite = this.GetComponent<SpriteRenderer>();
+        currSpriteColor = this.GetComponent<SpriteRenderer>().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currSpriteColor.r = (255 + Mathf.Sin(Time.deltaTime));
+	}
+}
